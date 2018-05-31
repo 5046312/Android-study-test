66ab7269a78ed5d2340f4bf1771f949c8e9ba86d
5046312 <5046312@qq.com>
2018-05-31 21:07:24 +0800

    准备开始ProgressBar

--- app/src/main/java/com/example/first/studytest/MainActivity.java
+++ app/src/main/java/com/example/first/studytest/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.first.studytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.first.studytest.ProgressBar.ProgressBarActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn = findViewById(R.id.btn_ProgressBar);
+        btn.setOnClickListener(new BtnClick());
+    }
+
+    class BtnClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            switch (v.getId()) {
+                case R.id.btn_ProgressBar:
+                    intent.setClass(MainActivity.this, ProgressBarActivity.class);
+                    break;
+            }
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/first/studytest/ProgressBar/ProgressBarActivity.java
@@ -0,0 +1,16 @@
+package com.example.first.studytest.ProgressBar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.first.studytest.R;
+
+public class ProgressBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTitle("ProgressBar");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress_bar);
+    }
+}
