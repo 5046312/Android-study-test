ce3f0d426d48cbb199fb80f683b070957a3ebbf3
5046312 <5046312@qq.com>
2018-05-31 22:17:26 +0800

    hard

---
+++ app/src/main/java/com/example/first/studytest/ProgressBar/ProgressBarNormalActivity.java
@@ -0,0 +1,13 @@
+package com.example.first.studytest.ProgressBar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProgressBarNormalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress_bar_normal);
+    }
+}
