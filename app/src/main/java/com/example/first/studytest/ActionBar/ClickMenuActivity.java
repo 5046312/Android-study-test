4d86d9b5febd060324203980e73d6159eb38e22c
5046312 <5046312@qq.com>
2018-06-08 10:59:07 +0800

    主要是 menu 的3种用法

--- app/src/main/java/com/example/first/studytest/ActionBar/ClickMenuActivity.java
+++ app/src/main/java/com/example/first/studytest/ActionBar/ClickMenuActivity.java
@@ -1,11 +1,16 @@
 package com.example.first.studytest.ActionBar;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.example.first.studytest.R;
 
@@ -17,6 +22,10 @@ public class ClickMenuActivity extends AppCompatActivity implements View.OnClick
         setContentView(R.layout.activity_click_menu);
         Button btn = findViewById(R.id.click_menu_tooltip);
         btn.setOnClickListener(this);
+
+        // 上下文菜单
+        btn = findViewById(R.id.click_menu_context);
+        registerForContextMenu(btn);
     }
 
     @Override
@@ -25,8 +34,41 @@ public class ClickMenuActivity extends AppCompatActivity implements View.OnClick
             case R.id.click_menu_tooltip:
                 PopupMenu popupMenu = new PopupMenu(ClickMenuActivity.this, v);
                 getMenuInflater().inflate(R.menu.normal_menu, popupMenu.getMenu());
+
+                // 设置监听
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.menu_item_1:
+                                Toast.makeText(ClickMenuActivity.this, "点击了", Toast.LENGTH_LONG).show();
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
                 popupMenu.show();
                 break;
         }
     }
+
+    @SuppressLint("ResourceType")
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater menuInflater = new MenuInflater(ClickMenuActivity.this);
+        menuInflater.inflate(R.menu.normal_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_item_1:
+                Toast.makeText(ClickMenuActivity.this, "点击", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return true;
+    }
 }
+
--- app/src/main/java/com/example/first/studytest/ActionBar/NormalActionBarActivity.java
+++ app/src/main/java/com/example/first/studytest/ActionBar/NormalActionBarActivity.java
@@ -4,20 +4,54 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.first.studytest.R;
 
-public class NormalActionBarActivity extends AppCompatActivity {
+public class NormalActionBarActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal_action_bar);
+
+        Button button = findViewById(R.id.refreshActionBar);
+        button.setOnClickListener(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.normal_action_bar_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_bar_next_item:
+                Toast.makeText(NormalActionBarActivity.this, "点击下一个", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // 切换 下一个 按钮
+        menu.findItem(R.id.action_bar_next_item).setTitle(Math.random() + "随机");
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.refreshActionBar:
+                invalidateOptionsMenu();//通知系统刷新Menu
+                break;
+        }
     }
 }
