041bc12e96856405fdb0276bd641274acf30601c
5046312 <5046312@qq.com>
2018-06-11 18:04:41 +0800

    bluetooth todo

--- app/src/main/java/com/example/first/studytest/BlueTooth/BlueToothMainActivity.java
+++ app/src/main/java/com/example/first/studytest/BlueTooth/BlueToothMainActivity.java
@@ -2,10 +2,15 @@ package com.example.first.studytest.BlueTooth;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,16 +20,22 @@ import android.widget.Button;
 
 import com.example.first.studytest.R;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import xyz.bboylin.universialtoast.UniversalToast;
 
 public class BlueToothMainActivity extends AppCompatActivity implements View.OnClickListener {
     boolean openBT = false; // 开启蓝牙
     BluetoothAdapter blueadapter;
+    AlertDialog.Builder builder;
+
+    Button btn_scan; // 搜索按钮
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,8 +44,8 @@ public class BlueToothMainActivity extends AppCompatActivity implements View.OnC
         Button btn = findViewById(R.id.open_bt);
         btn.setOnClickListener(this);
 
-        btn = findViewById(R.id.scan_bt);
-        btn.setOnClickListener(this);
+        btn_scan = findViewById(R.id.scan_bt);
+        btn_scan.setOnClickListener(this);
 
         blueadapter = BluetoothAdapter.getDefaultAdapter();
 
@@ -43,6 +54,9 @@ public class BlueToothMainActivity extends AppCompatActivity implements View.OnC
         intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
         intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
         registerReceiver(broadcastReceiver, intentFilter);
+
+        // 显示alertDialog
+        builder = new AlertDialog.Builder(BlueToothMainActivity.this);
     }
 
     @Override
@@ -75,11 +89,14 @@ public class BlueToothMainActivity extends AppCompatActivity implements View.OnC
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 showDeviceList(device);
             }else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
-                UniversalToast.makeText(BlueToothMainActivity.this, "搜索结束", UniversalToast.LENGTH_SHORT).show();
+                // 搜索结束
+                btn_scan.setEnabled(true);
+                btn_scan.setText("搜索设备");
             }
         }
     };
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -88,6 +105,8 @@ public class BlueToothMainActivity extends AppCompatActivity implements View.OnC
                 startActivityForResult(enabler, 1);
                 break;
             case R.id.scan_bt:
+                btn_scan.setEnabled(false);
+                btn_scan.setText("搜索中...");
                 deviceList.clear();
                 blueadapter.startDiscovery();
                 break;
@@ -114,7 +133,49 @@ public class BlueToothMainActivity extends AppCompatActivity implements View.OnC
     // 显示搜索到的设备
     void showDeviceList(BluetoothDevice device){
         deviceList.add(device);
-        // 显示alertDialog
-        
+        builder.setTitle("选择蓝牙设备");
+        List<String> mDeviceName = new ArrayList<>();
+        for (BluetoothDevice btDevice: deviceList){
+            mDeviceName.add(btDevice.getName());
+        }
+        String[] deviceName = mDeviceName.toArray(new String[mDeviceName.size()]);
+        builder.setItems(deviceName, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // 取消搜索
+                if (blueadapter != null && blueadapter.isDiscovering()) {
+                    System.out.println("取消搜索");
+                    blueadapter.cancelDiscovery();
+
+                }
+
+                connectDevice(deviceList.get(which));
+//                UniversalToast.makeText(BlueToothMainActivity.this, address, UniversalToast.LENGTH_SHORT).show();
+            }
+        });
+        builder.show();
+    }
+
+    BluetoothDevice device;
+
+    void connectDevice(BluetoothDevice device) {
+        BluetoothDevice bluetoothDevice = blueadapter.getRemoteDevice(device.getAddress());
+
+
+        try {
+            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+            BluetoothSocket bluetoothSocket = device.createRfcommSocketToServiceRecord(uuid);
+            bluetoothSocket.connect();
+            OutputStream outputStream = bluetoothSocket.getOutputStream();
+
+
+            String str = "hello world";
+            byte[] data = str.getBytes("gbk");
+            outputStream.write(data, 0, data.length);
+            outputStream.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
